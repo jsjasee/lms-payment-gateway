8cd7afd009e59bff7392044805205a0a373a47da
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-04-04 15:15:19 +0800

    add security packages to codebase

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import { configDotenv } from "dotenv";
 import morgan from "morgan";
 import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 configDotenv(); // our .env path is in the root of the home folder, so no additional config needed.
 
 const app = express();
 const PORT = process.env.PORT;
+
+// security - global rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "Too many requests from this IP, please try later.",
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+// middleware for security (but only apply it for those routes that starts with /api)
+app.use("/api", limiter);
+app.use(ExpressMongoSanitize());
+app.use(hpp());
+app.use(helmet()); // this one secure Express apps by setting HTTP response headers
 
 // middleware for logging
 if (process.env.NODE_ENV === "development") {
@@ -16,6 +39,7 @@ if (process.env.NODE_ENV === "development") {
 // Body parser middleware
 app.use(express.json({ limit: "10kb" })); // we are accepting json files but limiting size to 10kb
 app.use(express.urlencoded({ extended: true, limit: "10kb" })); // extended means use the latest version, urlencoded helps us convert spaces to %20
+app.use(cookieParser());
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -26,6 +50,25 @@ app.use((err, req, res, next) => {
     ...(process.env.NODE_ENV === "development" && { stack: err.stack }), // only unwrap when it is in development, && means both need to be true in order to dump the entire stack.
   });
 });
+
+// CORS configuration
+// app.use(cors()) ❌
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173", // "*" ❌ this * means accept req from every origin.. dangerous
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"], // what requests can frontend send us
+    allowedHeaders: [
+      "Authorization",
+      "Content-Type",
+      "X-Requested-With",
+      "device-remember-token",
+      "Access-Control-Allow-Origin",
+      "Origin",
+      "Accept",
+    ],
+  }),
+);
 
 // API routes
 
